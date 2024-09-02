b1d0437aca10d8fd3b481367298ba7a48238aa2f
tonywowai <tonyshark365@gmail.com>
2024-09-02 21:25:24 +0700

    .

--- clustermesh/install-metallb.sh
+++ clustermesh/install-metallb.sh
@@ -3,10 +3,10 @@
 set -e
 set -o pipefail
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.5/config/manifests/metallb-native.yaml --context=kind-kind-1
-kubectl wait deployment -n metallb-system controller --for condition=Available=True --timeout=90s --context kind-kind-1
-kubectl apply -f metallb-1.yaml --context=kind-kind-1
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.5/config/manifests/metallb-native.yaml --context=kind-cluster1
+kubectl wait deployment -n metallb-system controller --for condition=Available=True --timeout=90s --context kind-cluster1
+kubectl apply -f metallb-1.yaml --context=kind-cluster1
 
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.5/config/manifests/metallb-native.yaml --context=kind-kind-2
-kubectl wait deployment -n metallb-system controller --for condition=Available=True --timeout=90s --context kind-kind-2
-kubectl apply -f metallb-2.yaml --context=kind-kind-2
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.5/config/manifests/metallb-native.yaml --context=kind-cluster2
+kubectl wait deployment -n metallb-system controller --for condition=Available=True --timeout=90s --context kind-cluster2
+kubectl apply -f metallb-2.yaml --context=kind-cluster2
