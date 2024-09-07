8ce78d88f77a9e49c285e2fd26e462e9a8cbce62
tonywowai <tonyshark365@gmail.com>
2024-09-07 19:44:25 +0700

    .

--- clustermesh/generate-certs.sh
+++ clustermesh/generate-certs.sh
@@ -13,7 +13,7 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
                -out certs/ca.crt \
                -config ca.conf
 
-sudo cp ca.crt /usr/share/ca-certificates/
+sudo cp certs/ca.crt /usr/share/ca-certificates/
 su -
 dpkg-reconfigure ca-certificates
 
