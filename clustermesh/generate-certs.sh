cdfc0e0db1522735374cc18bf15afdeadfb97c8a
tonywowai <tonyshark365@gmail.com>
2024-09-07 23:52:03 +0700

    .

--- clustermesh/generate-certs.sh
+++ clustermesh/generate-certs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# https://gist.github.com/nathanielks/88e7827a653e8696021ed436481b42dc
+# https://kubernetes.io/docs/tasks/administer-cluster/certificates/
 mkdir -p certs
 
 # openssl req -x509 -new  -sha512 -key  certs/ca.key -config ca.conf -days 365 -out certs/ca.crt
@@ -17,6 +18,11 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 # su -
 # dpkg-reconfigure ca-certificates
 
+  # 344  openssl verify certs/ca.crt
+  # 345  openssl verify certs/clustermesh-admin.crt
+  # 346  openssl verify certs/ca.crt
+  # https://stackoverflow.com/questions/13295585/openssl-certificate-verification-on-linux
+  
 certs=(
     "clustermesh-server" "clustermesh-admin" "clustermesh-client"
 )
@@ -27,7 +33,9 @@ for i in ${certs[*]}; do
   openssl req -new -key "certs/${i}.key" -sha256 \
     -config "ca.conf" \
     -out "certs/${i}.csr"
-  openssl x509 -req -in "certs/${i}.csr" -CA "certs/ca.crt" -CAkey "certs/ca.key" -CAcreateserial -out "certs/${i}.crt" -days 3650  -extfile ca.conf 
+  openssl x509 -req -days 3650 -in "certs/${i}.csr" -signkey "certs/${i}.key" -out "certs/${i}.crt"
+
+  # openssl x509 -req -in "certs/${i}.csr" -CA "certs/ca.crt" -CAkey "certs/ca.key" -CAcreateserial -out "certs/${i}.crt" -days 3650  -extfile ca.conf 
 
   # openssl x509 -req -days 3650 -in "certs/${i}.csr" \
   #   -sha256 -CA "certs/ca.crt" \
