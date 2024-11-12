cc65b306c85654fde2125b1742fdf0e5e2186d49
tonywowai <tonyshark365@gmail.com>
2024-11-12 07:49:39 +0700

    .

--- sn-development-k8s/redis/install.sh
+++ sn-development-k8s/redis/install.sh
@@ -8,3 +8,5 @@ chown 10001:10001 /storage/
 kubectl get pv
 helm upgrade redis-1669709819 bitnami/redis --set auth.enabled=true
 kubectl apply -f redis-commander.yaml
+
+# https://www.techtarget.com/searchitoperations/tutorial/Deploy-a-Redis-Cluster-on-Kubernetes
